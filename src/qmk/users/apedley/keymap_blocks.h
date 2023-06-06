e257344e8e99e8d3d879897a08276c0c53f3bb72
apedley <apedley@gmail.com>
2023-06-06 16:03:29 -0500

    misc userspace

---
+++ src/qmk/users/apedley/keymap_blocks.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+#include "keycodes.h"
+
+// clang-format off
+
+#define ____NUMPAD_1____  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS
+#define ____NUMPAD_2____  KC_P7,    KC_P8,    KC_P9,    KC_PPLS
+#define ____NUMPAD_3____  KC_P4,    KC_P5,    KC_P6,    KC_PENT
+#define ____NUMPAD_4____  KC_P1,    KC_P2,    KC_P3
+#define ____NUMPAD_5____  _______   KC_P0,    KC_PDOT
+
+// clang-format on
