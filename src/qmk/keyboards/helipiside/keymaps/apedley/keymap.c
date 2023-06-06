1025ad12303da14566689b4aae920dadd6717208
apedley <apedley@gmail.com>
2023-06-06 14:59:08 -0500

    added helipiside

---
+++ src/qmk/keyboards/helipiside/keymaps/apedley/keymap.c
@@ -0,0 +1,80 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include "apedley.h"
+#include QMK_KEYBOARD_H
+enum layer_number {
+  _BASE,
+  _FN,
+  _FN2
+};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Default layer
+   *        ,-----------------------------------------.
+   *        |   6  |   7  |   8  |   9  |   0  | Bksp |
+   *        |------+------+------+------+------+------|
+   *        |   Y  |   U  |   I  |   O  |   P  |  \   |
+   *        |------+------+------+------+------+------|
+   *        |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * ,------+------+------+------+------+------+------|
+   * |   ]  |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * /------+------+------+------+------+------+------|
+   * |  SPC |  ALT |  CTL | Left | Down |  Up  |Right |
+   * -------------------------------------------------'
+   */
+
+  [_BASE] = LAYOUT(
+              KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,  KC_HOME, KC_DEL,
+              KC_P7,    KC_P8,    KC_P9,    KC_PPLS,  KC_END,  KC_VOLU,
+              KC_P4,    KC_P5,    KC_P6,    KC_PENT,  KC_PGUP, KC_MUTE,
+    TT(_FN2), KC_P1,    KC_P2,    KC_P3,    KC_RSFT,  KC_PGDN, KC_VOLD,
+    TT(_FN),  KC_RCTL,  KC_P0,    KC_PDOT,  KC_RALT,  KC_RGUI, TT(_FN2)
+  ),
+  [_FN] = LAYOUT(
+              RGB_MOD, RGB_TOG, RGB_VAI, RGB_HUI, _______, _______,
+              _______, _______, _______, _______, _______, _______,
+              _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______,
+    _______,  QK_BOOT, _______, _______, _______, _______, _______
+  ),
+  [_FN2] = LAYOUT(
+              RGB_MOD, RGB_TOG, RGB_VAI, RGB_HUI, _______, _______,
+              _______, _______, _______, _______, _______, _______,
+              _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______,
+    _______,  QK_BOOT, _______, _______, _______, _______, _______
+  )
+};
+
+#ifdef OLED_ENABLE
+void render_layer(void) {
+
+    char buf[10];
+
+  // Define layers here, Have not worked out how to have text displayed for each layer. Copy down the number you see and add a case for it below
+    switch (get_highest_layer(layer_state)) {
+    case _BASE:
+        oled_write_P(PSTR("Base"), false);
+        break;
+    case _FN:
+        oled_write_P(PSTR("FN1"), false);
+        break;
+    case _FN2:
+        oled_write_P(PSTR("FN2"), false);
+        break;
+    default:
+        oled_write_P(PSTR("Undef-"), false);
+        snprintf(buf,sizeof(buf), "%u", layer_state);
+        oled_write(buf, false);
+    }
+    oled_advance_page(true);
+}
+
+bool oled_task_keymap(void) {
+  render_layer();
+  render_rgblight(false);
+  render_status();
+  return false;
+}
+
+#endif
