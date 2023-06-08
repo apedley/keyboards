e004aa63c878cdc3f55d04a5b1630ba29687dc83
apedley <apedley@gmail.com>
2023-06-08 04:45:52 -0500

    l + semi colon = backspace

--- src/qmk/users/apedley/combos.c
+++ src/qmk/users/apedley/combos.c
@@ -7,6 +7,7 @@ enum combos {
     QW_ESC,
     DF_TAB,
     PBSLS_BSPC,
+    LSCLN_BSPC,
     IO_MINUS,
     OP_EQUAL,
     COMMA_DOT,
@@ -19,6 +20,7 @@ uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM pbsls_combo[] = {KC_P, KC_BSLS, COMBO_END};
+const uint16_t PROGMEM lscln_combo[] = {KC_L, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM io_minus_combo[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM op_equal_combo[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM comma_dot_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
@@ -28,6 +30,7 @@ combo_t key_combos[] = {
     [QW_ESC] = COMBO(qw_combo, KC_ESC),
     [DF_TAB] = COMBO(df_combo, KC_TAB),
     [PBSLS_BSPC] = COMBO(pbsls_combo, KC_BSPC),
+    [LSCLN_BSPC] = COMBO(lscln_combo, KC_BSPC),
     [IO_MINUS] = COMBO(io_minus_combo, KC_MINUS),
     [OP_EQUAL] = COMBO(op_equal_combo, KC_EQUAL),
     [COMMA_DOT] = COMBO_ACTION(comma_dot_combo),
