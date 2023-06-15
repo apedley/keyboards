4674f18546f5e31a53fae133153cef2780269950
apedley <apedley@gmail.com>
2023-06-15 05:10:56 -0500

    helixpad default

--- src/qmk/keyboards/helixpad/helixpad.c
+++ src/qmk/keyboards/helixpad/helixpad.c
@@ -1,5 +1,77 @@
-
+#include "helixpad.h"
 #include "quantum.h"
+
+#ifdef OLED_ENABLE
+
+// oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+//     return OLED_ROTATION_90;
+// }
+
+
+void render_layer_kb(void) {
+
+    char buf[10];
+
+    oled_write_P(PSTR("Layer: "), false);
+    snprintf(buf,sizeof(buf), "%u", layer_state);
+    oled_write(buf, false);
+    oled_advance_page(true);
+
+}
+void render_status_kb(void) {
+
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUML") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCRL") : PSTR("    "), false);
+    oled_advance_page(true);
+}
+
+
+void render_rgbmatrix_kb(bool simple) {
+#ifdef RGB_MATRIX_ENABLE
+
+  char buf[30];
+
+  if (rgb_matrix_is_enabled()) {
+
+    if (simple) {
+      snprintf(buf, sizeof(buf), "[%2d] ", rgb_matrix_get_mode());
+    } else {
+      snprintf(buf, sizeof(buf), "LED %2d: %d,%d,%d %d",
+                      rgb_matrix_get_mode(),
+                      rgb_matrix_get_hue()/8,
+                      rgb_matrix_get_sat()/16,
+                      rgb_matrix_get_val()/16,
+                      rgb_matrix_get_speed()/26);
+      oled_write(buf, false);
+    }
+  }
+
+  oled_advance_page(true);
+#endif
+}
+
+bool oled_task_kb(void) {
+  if (oled_task_user()) {
+    render_layer_kb();
+    render_rgbmatrix_kb(false);
+    render_status_kb();
+  }
+  return false;
+}
+#endif // OLED_ENABLE
+
+void keyboard_post_init_kb() {
+
+#ifdef CONSOLE_ENABLE
+  debug_config.enable = true;
+  // debug_config.keyboard = true;
+  // debug_config.matrix = true;
+#endif // CONSOLE_ENABLE
+
+  keyboard_post_init_user();
+}
 
 #ifdef RGB_MATRIX_ENABLE
 
@@ -7,7 +79,7 @@
 
 led_config_t g_led_config = { {
   //Key Matrix to LED Index
-  { 5,  4,  3,  2,  1,  0, NO_LED},
+  { 5,  4,  3,  2,  1,  0, NO_LED },
   { 6,  7,  8,  9, 10, 11, NO_LED },
   {17, 16, 15, 14, 13, 12, NO_LED },
   {18, 19, 20, 21, 22, 23, 24},
@@ -16,19 +88,20 @@ led_config_t g_led_config = { {
 
 
   //LED Index to Physical Positon
-                  { 37,   0}, {  74,   0}, { 111,   0}, { 148,   0}, {185,  0}, { 222,   0},
-                  { 222, 16}, { 185,  16}, { 148,  16}, { 111,  16}, { 74, 16}, {  37,  16},
-                  { 37,  32}, {  74,  32}, { 111,  32}, { 148,  32}, {185, 32}, { 222,  32},
-      { 222, 48}, { 185, 48}, { 148,  48}, { 111,  48}, {  74,  48}, { 37, 48}, {   0,  48},
-      {   0, 64}, { 37,  64}, {  74,  64}, { 111,  64}, { 148,  64}, {185, 64}, { 222,  64}
+                  { 86,  0}, { 69,  0}, { 52,  0}, { 34, 0}, { 17,  0}, {  0, 0},
+                  { 0,   16}, { 17,  16}, { 34,  16}, { 52, 16}, { 69,  16}, { 86, 16},
+                  { 86,  32}, { 69,  32}, { 52,  32}, { 34, 32}, { 17,  32}, {  0, 32},
+      {   0, 48}, { 17,  48}, { 34,  48}, { 52,  48}, { 69, 48}, { 86,  48}, { 103,48},
+      { 103, 64}, { 86,  64}, { 69,  64}, { 52,  64}, { 34, 64}, { 17,  64}, {  0, 64},
 
 }, {
   // LED Index to Flag
-     4, 4, 4, 4, 4, 4,
+     1, 4, 4, 4, 4, 4,
      4, 4, 4, 4, 4, 4,
      4, 4, 4, 4, 4, 4,
   4, 4, 4, 4, 4, 4, 1,
-  1, 4, 4, 4, 4, 1, 1
+  1, 4, 4, 4, 4, 1, 1,
+
 
 } };
 
--- src/qmk/keyboards/helixpad/keymaps/default/keymap.c
+++ src/qmk/keyboards/helixpad/keymaps/default/keymap.c
@@ -1,5 +1,11 @@
 #include QMK_KEYBOARD_H
 
+enum layer_number {
+  _BASE,
+  _FN1
+};
+
+#define FN1 TT(_FN1)
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,13 +24,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * -------------------------------------------------'
    */
 
-    [0] = LAYOUT(
-                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_Z,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-      KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7
-    )
+  [_BASE] = LAYOUT(
+              KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,  KC_1,    KC_2,
+              KC_P7,    KC_P8,    KC_P9,    KC_PPLS,  KC_VOLU, QK_REP,
+              KC_P4,    KC_P5,    KC_P6,    KC_PENT,  KC_MUTE, QK_AREP,
+    FN1,      KC_P1,    KC_P2,    KC_P3,    KC_RSFT,  KC_VOLD, QK_REP,
+    FN1,      KC_RCTL,  KC_P0,     KC_PDOT,  KC_RALT,  FN1,     FN1
+  ),
+  [_FN1] = LAYOUT(
+              KC_CAPS, _______, _______, _______, _______, _______,
+              RGB_MOD, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,
+              RGB_RMOD,RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,
+    _______,  _______, _______, _______, _______, _______, QK_RBT,
+    _______,  _______, _______, _______, _______, _______, QK_BOOT
+  )
 };
 
 // clang-format on
