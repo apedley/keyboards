64157adcae828a4910b35d047426f8b43a2a4120
apedley <apedley@gmail.com>
2023-06-16 14:48:35 -0500

    mykmp

--- src/qmk/keyboards/fingerpunch/ximi/keymaps/apedley/keymap.c
+++ src/qmk/keyboards/fingerpunch/ximi/keymaps/apedley/keymap.c
@@ -2,6 +2,7 @@
 #include "apedley.h"
 #include QMK_KEYBOARD_H
 
+
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
@@ -42,40 +43,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                                           KC_PGDN,      KC_END,       KC_PGUP,      KC_VOLD, KC_MUTE,      KC_VOLU
     // ),
     [_BASE] = LAYOUT_ximi(
-    QK_GESC,  KC_Q,   KC_W,   KC_E,     KC_R,   KC_T,         KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,   KC_BSPC,
-    LOWER,    KC_A,   KC_S,   KC_D,     KC_F,   KC_G,         KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN,KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,   KC_C,     KC_V,   KC_B,         KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,KC_RGUI,
-                KC_MUTE,      KC_LCTL,  KC_ENT, KC_SPC,       KC_RALT, KC_SPC,  RAISE,          KC_MUTE,
-                              KC_WH_D,  KC_BTN3,KC_WH_U,      KC_WH_D,  KC_BTN3,KC_WH_U
+    KC_ESC,   KC_Q,     KC_W,   KC_E,     KC_R,   KC_T,            KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,   KC_BSPC,
+    LOWER,    KC_A,     KC_S,   KC_D,     KC_F,   KC_G,            KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN,KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,   KC_C,     KC_V,   KC_B,            KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,KC_RGUI,
+                KC_MUTE,        KC_TAB,    KC_LCTL,KC_SPC,         KC_ENT,  KC_RALT, RAISE,          KC_MUTE,
+                                KC_WH_D,  KC_BTN3,KC_WH_U,         KC_WH_D, KC_BTN3, KC_WH_U
     ),
     [_RAISE] = LAYOUT_ximi(
-    KC_ESC,      KC_1,    KC_UP,   KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    QK_RBT,      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-    QK_BOOT,     KC_CAPS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,      _______, _______, _______, KC_PIPE,  KC_DQT, _______,
-                    QK_BOOT,       _______, _______, _______,      QK_BOOT, KC_MAKE, QK_RBT,          _______,
-                                   _______, _______, _______,      KC_VOLD, KC_MUTE, KC_VOLU
+    KC_GRV,   KC_1,    KC_UP,   KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______,   KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    QK_BOOT,  KC_CAPS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,        _______, _______, _______, KC_PIPE,  KC_DQT, _______,
+                 QK_BOOT,       _______, _______, _______,        QK_BOOT, KC_MAKE, _______,          _______,
+                                _______, _______, _______,        KC_VOLD, KC_MUTE, KC_VOLU
     ),
     [_LOWER] = LAYOUT_ximi(
-    _______,      KC_1,    KC_UP,   KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______,      KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    _______,      _______, _______, _______, _______, _______,      _______, _______, _______, KC_BSLS, KC_QUOT, _______,
-                    _______,        QK_RBT,  QK_BOOT, KC_MAKE,      _______, _______, _______,        _______,
-                                    KC_BTN1, KC_BTN3, KC_BTN2,      KC_VOLD, KC_MUTE, KC_VOLU
+    KC_GRV,   KC_1,    KC_UP,   KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______,  KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_TAB,        _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+    _______,  _______, _______, _______, _______, DB_TOGG,       _______, _______, _______, KC_BSLS, KC_QUOT, _______,
+                 QK_BOOT,       QK_RBT,  QK_BOOT, KC_MAKE,       _______, _______, _______,        _______,
+                                KC_BTN1, KC_BTN3, KC_BTN2,       KC_VOLD, KC_MUTE, KC_VOLU
     ),
 
     [_ADJUST] =  LAYOUT_ximi(
-    _______,      RGB_TOG, RGB_RMOD, RGB_MOD, _______, TO(_BASE),       KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
-    _______,      RGB_SPI, RGB_HUI,  RGB_SAI, RGB_VAI, _______,         KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
-    _______,      RGB_SPD, RGB_HUD,  RGB_SAD, RGB_VAD, _______,         KC_F11,  KC_F12,  _______, QK_RBT,   QK_BOOT, _______,
-                    _______,         _______, _______, _______,         _______, _______, _______,          _______,
-                                     _______, _______, _______,         _______, _______, _______
+    _______,  RGB_TOG, RGB_RMOD, RGB_MOD, _______, TO(_BASE),    KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   _______,
+    _______,  RGB_SPI, RGB_HUI,  RGB_SAI, RGB_VAI, _______,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  _______,
+    _______,  RGB_SPD, RGB_HUD,  RGB_SAD, RGB_VAD, _______,      KC_F11,  KC_F12,  _______, QK_RBT, QK_BOOT, _______,
+                  QK_BOOT,       _______, _______, _______,      _______, _______, _______,        _______,
+                                 _______, _______, _______,      _______, _______, _______
     ),
 
     [_NUMPAD] =  LAYOUT_ximi(
-    _______,      _______, _______, _______, _______, _______,      KC_NUM,  KC_P7,   KC_P8,   KC_P9,  KC_PMNS,  KC_PSLS,
-    _______,      _______, _______, _______, _______, _______,      _______, KC_P4,   KC_P5,   KC_P6,  KC_PPLS,  KC_PAST,
-    _______,      _______, _______, _______, _______, _______,      _______, KC_P1,   KC_P2,   KC_P3,  KC_PENT,  _______,
-                    _______,        _______, _______, _______,      _______, KC_P0,   KC_PDOT,       _______,
-                                    _______, _______, _______,      _______, _______, _______
+    _______,  _______, _______, _______, _______, _______,       KC_NUM,  KC_P7,   KC_P8,   KC_P9,  KC_PMNS,  KC_PSLS,
+    _______,  _______, _______, _______, _______, _______,       _______, KC_P4,   KC_P5,   KC_P6,  KC_PPLS,  KC_PAST,
+    _______,  _______, _______, _______, _______, _______,       _______, KC_P1,   KC_P2,   KC_P3,  KC_PENT,  _______,
+                _______,        _______, _______, _______,       _______, KC_P0,   KC_PDOT,       _______,
+                                _______, _______, _______,       _______, _______, _______
     )
 };
+void keyboard_post_init_keymap(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
