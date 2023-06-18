4a3d31f9c748f04d41d63cd619dfb2cf20945023
apedley <apedley@gmail.com>
2023-06-18 03:14:16 -0500

    oled back

--- src/qmk/keyboards/fingerpunch/ximi/keymaps/apedley/rules.mk
+++ src/qmk/keyboards/fingerpunch/ximi/keymaps/apedley/rules.mk
@@ -3,15 +3,12 @@ RGB_MATRIX_ENABLE = yes
 AUDIO_ENABLE =yes
 ENCODER_ENABLE = yes
 HAPTIC_ENABLE = yes
-WPM_ENABLE = yes
-
-
 
 OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
-WPM_ENABLE = yes
 
 ifeq ($(strip $(OLED_ENABLE)), yes)
   SRC += oled_display.c
+	OLED_DRIVER = SSD1306
+	WPM_ENABLE = yes
 endif
 
