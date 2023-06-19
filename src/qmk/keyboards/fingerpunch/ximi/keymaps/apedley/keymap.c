0a6c80d6c5cdaa0d46b9cf8c96ab79f93c3c0e0e
apedley <apedley@gmail.com>
2023-06-19 02:20:16 -0500

    its an lcd baby

--- src/qmk/keyboards/fingerpunch/ximi/keymaps/apedley/keymap.c
+++ src/qmk/keyboards/fingerpunch/ximi/keymaps/apedley/keymap.c
@@ -1,4 +1,5 @@
 #include "apedley.h"
+#include "qp.h"
 #include QMK_KEYBOARD_H
 
 // float sonic_ring_song[][2] = SONG(SONIC_RING);
@@ -100,6 +101,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+#ifdef QUANTUM_PAINTER_ENABLE
+
+painter_device_t lcd;
+
+void lv_example_arc_2(void);
+
+#endif
+
 void keyboard_post_init_keymap(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
@@ -107,12 +116,52 @@ void keyboard_post_init_keymap(void) {
   // debug_keyboard=true;
   debug_mouse=true;
 
+  #ifdef QUANTUM_PAINTER_ENABLE
+
     // pimoroni_trackball_set_rgbw(0, 0, 254, 0);
+    lcd = qp_gc9a01_make_spi_device(240, 240, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 4, 0);
+    qp_init(lcd, QP_ROTATION_0);
+    qp_rect(lcd, 0, 0, 239, 319, 0, 255, 255, true);
+
+    if (qp_lvgl_attach(lcd)) {
+        lv_example_arc_2();
+    }
+  #endif
 }
 
+#ifdef QUANTUM_PAINTER_ENABLE
+
+
+static void set_angle(void* obj, int32_t v) {
+    lv_arc_set_value(obj, v);
+}
+
+/**
+ * Create an arc which acts as a loader.
+ */
+void lv_example_arc_2(void) {
+    /*Create an Arc*/
+    lv_obj_t* arc = lv_arc_create(lv_scr_act());
+    lv_arc_set_rotation(arc, 270);
+    lv_arc_set_bg_angles(arc, 0, 360);
+    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);  /*Be sure the knob is not displayed*/
+    lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE); /*To not allow adjusting by click*/
+    lv_obj_center(arc);
+
+    static lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, arc);
+    lv_anim_set_exec_cb(&a, set_angle);
+    lv_anim_set_time(&a, 1000);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE); /*Just for the demo*/
+    lv_anim_set_repeat_delay(&a, 500);
+    lv_anim_set_values(&a, 0, 100);
+    lv_anim_start(&a);
+}
 layer_state_t layer_state_set_keymap(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+#endif
 
 #ifdef OLED_ENABLE
 
