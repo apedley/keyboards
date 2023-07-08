0bd8d3503001b18cf8a85a4da0f8d76c96d5e380
apedley <apedley@gmail.com>
2023-07-07 19:31:20 -0500

    update config.h, keymap.c and keymap.sh

--- util/keymap.sh
+++ util/keymap.sh
@@ -14,6 +14,8 @@ VIZ_CONFIG=$XDG_CONFIG_HOME/keymapviz.conf
 
 keymapviz -k $KEYBOARD $KEYMAP_DIR/$KEYMAP_FILE -c $VIZ_CONFIG -t fancy -o $KEYMAP_DIR/km.txt
 paps -o $KEYMAP_DIR/km.pdf --landscape $KEYMAP_DIR/km.txt
-# # keymapviz -k ximi keyboards/fingerpunch/ximi/keymaps/apedley/keymap.c -c ~/.config/keymapviz.conf -t fancy | paps -o km.pdf --landscape
+
+echo To replace the keymap in the comments, run:
+echo "keymapviz -k ximi $KEYMAP_DIR/$KEYMAP_FILE -c $VIZ_CONFIG -r"
 
 # echo "keymapviz -k $KEYBOARD $KEYMAP_DIR/$KEYMAP_FILE -c $VIZ_CONFIG -t json -o $KEYMAP_DIR/km.json"
