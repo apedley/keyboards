59019c3c7d0adee74ed8fbf4fd271dc9ed480a9a
apedley <apedley@gmail.com>
2023-07-08 22:12:55 -0500

    ximi half leds

--- src/qmk/keyboards/fingerpunch/ximi/keymaps/ximihalf/keymap.c
+++ src/qmk/keyboards/fingerpunch/ximi/keymaps/ximihalf/keymap.c
@@ -4,7 +4,7 @@
 #include "print.h"
 
 #ifdef RAW_ENABLE
-  #include "raw_hid.h"
+#    include "raw_hid.h"
 #endif
 
 #include QMK_KEYBOARD_H
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ximi(
     _______,  _______, _______,  _______,  _______, _______,    _______, _______, _______, _______, _______, _______,
     _______,  _______, _______,  _______,  RAISE, _______,    _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______,  KC_1,  KC_2, KC_3,    _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______,  QK_BOOT,  RGB_MOD, QK_RBT,    _______, _______, _______, _______, _______, _______,
                  _______,       _______,  _______, _______,       _______, _______, _______,        _______,
                                 _______, _______, _______,         _______, _______, _______
     ),
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT_ximi(
     _______,  _______, _______,  _______,  _______, _______,    _______, _______, _______, _______, _______, _______,
     _______,  _______, _______,  _______,  _______, _______,    _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______,  _______,  _______, _______,    _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______,  QK_RBT,  KC_1, _______,    _______, _______, _______, _______, _______, _______,
                  _______,       _______,  _______, _______,       _______, _______, _______,        _______,
                                 _______, _______, _______,         _______, _______, _______
     ),
@@ -144,53 +144,51 @@ void lv_example_arc_2(void);
 #endif
 
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable = true;
-  // debug_matrix=true;
-  // debug_keyboard=true;
-  // debug_mouse = true;
+    // Customise these values to desired behaviour
+    debug_enable = true;
+    // debug_matrix=true;
+    // debug_keyboard=true;
+    // debug_mouse = true;
 
 #ifdef QUANTUM_PAINTER_ENABLE
 
-  // pimoroni_trackball_set_rgbw(0, 0, 254, 0);
-  lcd = qp_gc9a01_make_spi_device(240, 240, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 4, 0);
-  // lcd = qp_st7789_make_spi_device(240, 310, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 4, 0);
-  qp_init(lcd, QP_ROTATION_0);
-  qp_rect(lcd, 0, 0, 239, 239, 0, 255, 255, true);
+    lcd = qp_gc9a01_make_spi_device(240, 240, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 4, 0);
+    qp_init(lcd, QP_ROTATION_0);
+    qp_rect(lcd, 0, 0, 239, 319, 0, 255, 255, true);
 
-  if (qp_lvgl_attach(lcd)) {
-    lv_example_arc_2();
-  }
+    if (qp_lvgl_attach(lcd)) {
+        lv_example_arc_2();
+    }
 #endif
 }
 
 #ifdef QUANTUM_PAINTER_ENABLE
 
-static void set_angle(void *obj, int32_t v) { lv_arc_set_value(obj, v); }
+static void set_angle(void* obj, int32_t v) {
+    lv_arc_set_value(obj, v);
+}
 
 /**
  * Create an arc which acts as a loader.
  */
 void lv_example_arc_2(void) {
-  /*Create an Arc*/
-  lv_obj_t *arc = lv_arc_create(lv_scr_act());
-  lv_arc_set_rotation(arc, 270);
-  lv_arc_set_bg_angles(arc, 0, 360);
-  lv_obj_remove_style(arc, NULL,
-                      LV_PART_KNOB); /*Be sure the knob is not displayed*/
-  lv_obj_clear_flag(arc,
-                    LV_OBJ_FLAG_CLICKABLE); /*To not allow adjusting by click*/
-  lv_obj_center(arc);
+    /*Create an Arc*/
+    lv_obj_t* arc = lv_arc_create(lv_scr_act());
+    lv_arc_set_rotation(arc, 270);
+    lv_arc_set_bg_angles(arc, 0, 360);
+    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);  /*Be sure the knob is not displayed*/
+    lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE); /*To not allow adjusting by click*/
+    lv_obj_center(arc);
 
-  static lv_anim_t a;
-  lv_anim_init(&a);
-  lv_anim_set_var(&a, arc);
-  lv_anim_set_exec_cb(&a, set_angle);
-  lv_anim_set_time(&a, 1000);
-  lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE); /*Just for the demo*/
-  lv_anim_set_repeat_delay(&a, 500);
-  lv_anim_set_values(&a, 0, 100);
-  lv_anim_start(&a);
+    static lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, arc);
+    lv_anim_set_exec_cb(&a, set_angle);
+    lv_anim_set_time(&a, 1000);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE); /*Just for the demo*/
+    lv_anim_set_repeat_delay(&a, 500);
+    lv_anim_set_values(&a, 0, 100);
+    lv_anim_start(&a);
 }
 #endif
 // #ifdef RAW_ENABLE
@@ -203,11 +201,10 @@ void lv_example_arc_2(void) {
 // }
 // #endif
 
-
 // layer_state_t layer_state_set_keymap(layer_state_t state) {
 //   state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 
-  // #ifdef RAW_ENABLE
+// #ifdef RAW_ENABLE
 //   uint8_t highest_layer = get_highest_layer(state);
 //   // uint8_t qmk_rc_layer_buffer[QMK_RC_BUFFER_MAX] = {0, 1, 0, 0, 0, 0, highest_layer, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0,   0, 0,   0, 0,  0, 0,0,   0, 0, 0, 0, 0,   0, 0,   0, 0,  0, 0,0,   0, 0, 0, 0, 0,   0, 0,   0, 0,  0, 0,0,   0, 0, 0, 0, 0,   0, 0,   0, 0,  0, 0,0,   0, 0, 0};
 
@@ -216,49 +213,49 @@ void lv_example_arc_2(void) {
 //   // }
 //   // raw_hid_send(qmk_rc_layer_buffer, QMK_RC_BUFFER_MAX);
 
-  // uint8_t data[32];
-  // data[0] = get_highest_layer(layer_state|default_layer_state);
+// uint8_t data[32];
+// data[0] = get_highest_layer(layer_state|default_layer_state);
 
-  // switch (get_highest_layer(state)) {
-  //   case _RAISE:
-  //     data[0] = 1;
-  //     break;
-  //   case _LOWER:
-  //     data[0] = 2;
-  //     break;
-  //   case _ADJUST:
-  //     data[0] = 3;
-  //     break;
-  //   case _NUMPAD:
-  //     data[0] = 4;
-  //     break;
-  //   default:
-  //     data[0] = 0;
-  //     break;
-  //   }
-  // raw_hid_send(data, 32);
-  // #endif
-  // #ifdef RAW_ENABLE
-  // uint8_t layer_data[32];
-  // layer_data[0] = 1;
-  // layer_data[1] = get_highest_layer(state|default_layer_state);
-  // raw_hid_send(layer_data, 32);
+// switch (get_highest_layer(state)) {
+//   case _RAISE:
+//     data[0] = 1;
+//     break;
+//   case _LOWER:
+//     data[0] = 2;
+//     break;
+//   case _ADJUST:
+//     data[0] = 3;
+//     break;
+//   case _NUMPAD:
+//     data[0] = 4;
+//     break;
+//   default:
+//     data[0] = 0;
+//     break;
+//   }
+// raw_hid_send(data, 32);
+// #endif
+// #ifdef RAW_ENABLE
+// uint8_t layer_data[32];
+// layer_data[0] = 1;
+// layer_data[1] = get_highest_layer(state|default_layer_state);
+// raw_hid_send(layer_data, 32);
 
-  // #endif // RAW_ENABLE
+// #endif // RAW_ENABLE
 
-  // #ifdef POINTING_DEVICE_ENABLE
-  // switch (get_highest_layer(state)) {
-  //   case _LOWER:
-  //   case _RAISE:
-  //     FP_SNIPE_ON
+// #ifdef POINTING_DEVICE_ENABLE
+// switch (get_highest_layer(state)) {
+//   case _LOWER:
+//   case _RAISE:
+//     FP_SNIPE_ON
 
-  //   case _ADJUST:
+//   case _ADJUST:
 
-  //   case _NUMPAD:
+//   case _NUMPAD:
 
-  //   default:
+//   default:
 
-  //   }
+//   }
 
 //   #endif
 // //   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
@@ -318,39 +315,38 @@ void lv_example_arc_2(void) {
 
 // #endif // OLED_ENABLE
 
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  case LOWER:
-    if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    }
-    return false;
-    break;
-  case RAISE:
-    if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    }
-    return false;
-    break;
-  case ADJUST:
-    if (record->event.pressed) {
-        layer_on(_ADJUST);
-    } else {
-        layer_off(_ADJUST);
-    }
-    return false;
-    break;
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case ADJUST:
+            if (record->event.pressed) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            return false;
+            break;
 #ifdef AUDIO_ENABLE
 #endif // AUDIO_ENABLE
-
-  }
-  return true;
+    }
+    return true;
 }
